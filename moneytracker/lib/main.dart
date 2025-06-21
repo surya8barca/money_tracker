34cfdd71c644e11070214d9fd7c582b51b49b63e
Surya <surya8barca@gmail.com>
2025-06-21 14:49:51 +0530

    [build] initial setup

--- moneytracker/lib/main.dart
+++ moneytracker/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final dir = await getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
+
   runApp(const MyApp());
 }
 
